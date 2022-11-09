8a85c640d2e154805da0da6ada125c4b9c0a34be
sergiogarciaa <98455070+sergiogarciaa@users.noreply.github.com>
2022-11-09 20:31:19 +0100

    Add files via upload

--- P21a_Garcia_Sergio.cs
+++ P21a_Garcia_Sergio.cs
@@ -1,69 +1,72 @@
 ﻿// Alumno: García Barrera, Sergio
 
-namespace P21a_Garcia_Sergio;
+using System;
 
-internal class P21a_Garcia_Sergio
+namespace P21a_Garcia_Sergio
 {
-    private static void Main(string[] args)
+    internal class P21_Garcia_Sergio
     {
-        int cant, min, num, nc;
-        bool esCorrecto;
-
-
-        cant = Captura("\n\tCantidad de múltiplos a presentar? [100..300]: ", 100, 300);
-
-        // Captura del valor inicial min
-        num = Captura("\n\t¿Número del que obtener sus múltiplos? [11..77]: ", 11, 77);
-
-        // Captura del valor inicial min
-        min = Captura("\n\t¿Valor mínimo? [1000..2000]: ", 1000, 2000);
-        // Captura del número de columnas en las que lo voy a presentar  nc
-
-        nc = Captura("\n\t¿Número de columnas? [3..8]: ", 3, 8);
-
-        // cálculo del primer múltiplo suponiendo que el límite inferior está incluido
-        var multiplo = min / num * num;
-
-        if (multiplo < min)
-            multiplo += num;
-
-        // presentación
-        Console.Clear();
-        Console.WriteLine("\n{0} primeros múltiplos de {1} a partir de {2} en {3} columnas", cant, num, min, nc);
-
-        for (var i = 0; i < cant; i++)
+        static void Main(string[] args)
         {
-            // cuando el contador sea múltiplo del nº de columnas...
-            // Salto de línea
-            if (i % nc == 0)
-                Console.WriteLine();
+            int cant, min, num, nc;
+            bool esCorrecto;
 
-            Console.Write("{0}\t", multiplo); // <-- Presentamos el múltiplo
-            multiplo += num; // <-- obtenemos el siguiente
+
+            cant = Captura("\n\tCantidad de múltiplos a presentar? [100..300]: ", 100, 300);
+
+            // Captura del valor inicial min
+            num = Captura("\n\t¿Número del que obtener sus múltiplos? [11..77]: ", 11, 77);
+
+            // Captura del valor inicial min
+            min = Captura("\n\t¿Valor mínimo? [1000..2000]: ", 1000, 2000);
+            // Captura del número de columnas en las que lo voy a presentar  nc
+
+            nc = Captura("\n\t¿Número de columnas? [3..8]: ", 3, 8);
+
+            // cálculo del primer múltiplo suponiendo que el límite inferior está incluido
+            int multiplo = (min / num) * num;
+
+            if (multiplo < min)
+                multiplo += num;
+
+            // presentación
+            Console.Clear();
+            Console.WriteLine("\n{0} primeros múltiplos de {1} a partir de {2} en {3} columnas", cant, num, min, nc);
+
+            for (int i = 0; i < cant; i++)
+            {
+                // cuando el contador sea múltiplo del nº de columnas...
+                // Salto de línea
+                if (i % nc == 0)
+                    Console.WriteLine();
+
+                Console.Write("{0}\t", multiplo); // <-- Presentamos el múltiplo
+                multiplo += num; // <-- obtenemos el siguiente
+            }
+
+
+            Console.Write("\n\n\tPulsa una tecla para salir");
+            Console.ReadKey();
         }
 
-
-        Console.Write("\n\n\tPulsa una tecla para salir");
-        Console.ReadKey();
-    }
-
-    private static int Captura(string txt, int min, int max)
-    {
-        var valor = 1;
-        bool ok;
-
-        do
+        static int Captura(string txt, int min, int max)
         {
-            Console.Write("\n\t {0}: ", txt);
-            ok = int.TryParse(Console.ReadLine(), out valor);
+            int valor = 1;
+            bool ok;
 
-            if (!ok) // IMPORTANTE: La expresión !ok equivale a ok == false
-                Console.Write("\n\t** Error!! el valor introducido no es un número entero **\n");
+            do
+            {
+                Console.Write("\n\t {0}: ", txt);
+                ok = Int32.TryParse(Console.ReadLine(), out valor);
 
-            else if (valor < min || valor > max)
-                Console.Write("\n\t** Error!! número fuera de rango **\n");
-        } while (!ok || valor < min || valor > max);
+                if (!ok) // IMPORTANTE: La expresión !ok equivale a ok == false
+                    Console.Write("\n\t** Error!! el valor introducido no es un número entero **\n");
 
-        return valor;
+                else if (valor < min || valor > max)
+                    Console.Write("\n\t** Error!! número fuera de rango **\n");
+            } while (!ok || valor < min || valor > max);
+
+            return valor;
+        }
     }
 }
