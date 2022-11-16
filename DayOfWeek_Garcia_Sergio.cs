27754d7e7d92570414d4d70188b63d798392e46f
sergiogarciaa <98455070+sergiogarciaa@users.noreply.github.com>
2022-11-16 13:15:19 +0100

    Add files via upload

---
+++ DayOfWeek_Garcia_Sergio.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diadelasemana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, sumaDia, nuevoDia;
+
+            Console.Write("\n\tIntroduce un dia de la semana del 0 (domingo) al 6 (sabado): ");
+            dia = Convert.ToInt32(Console.ReadLine());
+
+            switch (dia)
+            {
+                case 0: Console.Write("\n\tHoy es Domingo");
+                    break;
+                case 1: Console.Write("\n\tHoy es Lunes");
+                    break;
+                case 2: Console.Write("\n\tHoy es Martes");
+                    break;
+                case 3: Console.Write("\n\tHoy es Miercoles");
+                    break;
+                case 4: Console.Write("\n\tHoy es Jueves");
+                    break;
+                case 5: Console.Write("\n\tHoy es Viernes");
+                    break;
+                case 6: Console.Write("\n\tHoy es Sabado");
+                    break;
+                default: Console.Write("\n\tDía fuera de rango");
+                    break;
+            }
+
+            Console.Write("\n\tIntroduce cuantos dias quiere avanzar: ");
+            sumaDia = Convert.ToInt32(Console.ReadLine());
+
+            nuevoDia = (dia + sumaDia) % 7;
+
+            if (nuevoDia == 0)
+                Console.Write("\n\tDentro de {0} dias sera domingo", sumaDia);
+            else if (nuevoDia == 1)
+                Console.Write("\n\tDentro de {0} dias sera lunes", sumaDia);
+            else if (nuevoDia == 2)
+                Console.Write("\n\tDentro de {0} dias sera martes", sumaDia);
+            else if (nuevoDia == 3)
+                Console.Write("\n\tDentro de {0} dias sera miercoles", sumaDia);
+            else if (nuevoDia == 4)
+                Console.Write("\n\tDentro de {0} dias sera jueves", sumaDia);
+            else if (nuevoDia == 5)
+                Console.Write("\n\tDentro de {0} dias sera viernes", sumaDia);
+            else if (nuevoDia == 6)
+                Console.Write("\n\tDentro de {0} dias sera sabado", sumaDia);
+
+            Console.Write("\n\tPulsa intro para salir...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P11A_Garcia_Sergio.cs
@@ -0,0 +1,40 @@
+﻿// Alumno: García Sergio
+
+/* Area y perímetro de un círculo
+
+ * Se introduce el radio de un círculo e imprimirá la longitud de su círculo de su circunferencia y su área
+ * longitud = 2 * pi * r        área = pi * r2.
+
+ */
+
+
+using System;
+using System.Text.RegularExpressions;
+
+
+namespace P11A_Circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int radio;
+            double perimetro, area;
+            String captura;
+
+
+            Console.WriteLine("Introduce el radio: ");
+            captura = Console.ReadLine();
+            radio = Convert.ToInt32(captura);
+            perimetro = 2 * 3.14 * radio;
+            area = 3.14 * radio;
+            Console.WriteLine("\nEl área de un círculo de radio = " + radio + " es " + area);
+            Console.WriteLine("\n El perimetro del circulo anterior es " + perimetro);
+            Console.Write("\n\n Pulsa Intro para salir");
+            Console.ReadLine();
+           // Console.WriteLine("El radio es {0}, la longitud es {1}", radio, longitud(longitud = 2 * PI * radio));
+
+
+        }
+    }
+}
