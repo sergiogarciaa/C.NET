34fe582412d38a80999cf285a6556ac56d11c4cb
Sergio García Barrera <98455070+sergiogarciaa@users.noreply.github.com>
2023-05-30 14:04:13 +0200

    Add files via upload

---
+++ CalculadoraPrima/ClasePrimo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPrima
+{
+    
+    internal class ClasePrimo
+    {
+        private int num = -1;
+
+        public void setNum(int newNum)
+        {
+            this.num = newNum;
+        }
+        public int getNum() { 
+            return num; 
+        }
+
+        public bool DimeSiesPrimo()
+        {
+            Boolean resultado = false;
+            int numdivisores = -1;
+            numdivisores = CuantosDivisores();
+
+            if (numdivisores == 0)
+            {
+                resultado = true;
+            }
+            return resultado;
+        }
+
+        private int CuantosDivisores()
+        {
+            int resultado = 0;
+
+            for (int i = 2; i < this.num; i++)
+            {
+                if (this.num % i == 0) resultado += 1;
+            }
+
+            return resultado;
+        }
+
+        /*public int PideNumero()
+        {
+            int resultado;
+            Console.WriteLine("Dime un numero: ");
+            resultado = Convert.ToInt32(Console.ReadLine());
+            return resultado;
+        }*/
+        public void PideNumero2()
+        {
+            Console.WriteLine("Dime un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+        public void SalDespacio()
+        {
+            Console.WriteLine("Pulsa Enter para salir.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CalculadoraPrima/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPrima
+{
+    internal class Program
+    {
+        static int num = -1;
+        static void Main(string[] args)
+        {
+            //1. Instanciar
+            ClasePrimo calculadora = new ClasePrimo();
+ 
+            //2. Pedir numero
+            calculadora.PideNumero2();
+            //3. Llamar a DimeSiesPrimo
+            Boolean esPrimo = calculadora.DimeSiesPrimo();
+            //4. Mensaje con resultado
+            if (esPrimo) Console.WriteLine(calculadora.getNum() + " SÍ QUE ES PRIMO.");
+            else Console.WriteLine(calculadora.getNum() + " VA A SER QUE NO.");
+
+            calculadora.SalDespacio();
+        }
+        
+
+    }
+}
