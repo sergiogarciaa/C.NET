889f20915bfc53fc60f9e218c27e2c0a256034cd
Sergio García Barrera <98455070+sergiogarciaa@users.noreply.github.com>
2023-06-13 14:18:18 +0200

    ListasBorrar

---
+++ OrdenarApellidos/OrdenarApellidos/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenarApellidos
+{
+    internal class Class1
+    {
+        // 1.- Objeto Persona 
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+
+
+        // ---------------------------------------------------------------------------------
+        public Class1()
+        {
+
+                Console.WriteLine("Para salir: [salir]");
+                Console.WriteLine("Introduce nombre: ");
+                nombre = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Introduce apellido: ");
+                apellido = Convert.ToString(Console.ReadLine());
+        }
+        // ---------------------------------------------------------------------------------
+        public void verDatos()
+        {
+            Console.WriteLine(" Nombre : " + this.nombre + " | Apellido : " + this.apellido);
+        }
+
+       
+        public void EliminarAlumno(List<Class1> listaAlumnos)
+        {
+            Class1 aBorrar = new Class1();
+            String name;
+
+            Console.WriteLine("Introduzca el nombre de la persona que quiere borrar");
+            name = Console.ReadLine();
+            if(name == aBorrar.nombre)
+            listaAlumnos.Remove(aBorrar);
+            
+        }
+    }
+}
---
+++ OrdenarApellidos/OrdenarApellidos/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenarApellidos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lista de Alumnos y Ordenar por apellido
+            // 1.- Instanciar.
+            Class1 class1 = new Class1();
+            List<Class1> listaAlumnos = new List<Class1>();
+
+            Boolean seguir = true;
+
+            // 2.- Llamada a métodos
+            while (seguir)
+            {
+                switch (eligeopcion())
+                {
+                    case 1:
+                        // Añadir alumno a la clase
+                        listaAlumnos.Add(class1);
+                        break;
+                    case 2:
+                        // Imprimir lista
+                        foreach (Class1 element in listaAlumnos)
+                        {
+                           element.verDatos();
+                        }
+                        break;
+                    case 3:
+                        // Borrar
+                        int i = 0;
+                        Console.WriteLine("Indique el nombre del alumno");
+                        String borrarNombre = Console.ReadLine();   
+                        for (i = 0; i < listaAlumnos.Count; i++)
+                        {
+                            if (listaAlumnos[i].nombre.Equals(borrarNombre))
+                            {
+                                listaAlumnos.RemoveAt(i);
+                            }
+                            if (listaAlumnos[i].nombre == null)
+                            {
+
+                            }
+                        }
+                                                
+                        break;
+                    default:
+                        seguir = false;
+                        break;
+                }
+            }
+        }
+
+        private static int eligeopcion()
+        {
+            int resultado = 0;
+            Console.WriteLine("1 Añadir");
+            Console.WriteLine("2 Listar");
+            Console.WriteLine("3 Borrar");
+            Console.WriteLine("0 Salir");
+            resultado = Convert.ToInt32(Console.ReadLine());
+            return resultado;
+        }
+    }
+}
