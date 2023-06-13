1df5ed915b5cbd4d04cd394dc65098e5963c5780
Sergio García Barrera <98455070+sergiogarciaa@users.noreply.github.com>
2023-06-13 13:03:44 +0200

    Ordenar de menor a mayor Lista

---
+++ ListasMartes13/ListasMartes13/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasMartes13
+{
+    internal class Class1
+    {
+        // Pide Numeros y agrega a la lista
+        public void PedirNumeros(List<int> listaEnteros)
+        {
+            bool seguir = true;
+            int numero;
+
+            do
+            {
+                Console.WriteLine("Introduce numeros enteros cuándo termines introduce [0] para salir: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (numero == 0)
+                {
+                    seguir = false;
+                    Console.WriteLine("---------------------------------------");
+                }
+                else
+                {
+                    listaEnteros.Add(numero);
+                }
+            } while (seguir);
+        }
+
+        // Ordena Lista de menor a mayor
+        public void ComprobarLista(List<int> listaEnteros)
+        {
+            int aux;
+
+
+            for (int i = 0; i < listaEnteros.Count; i++)
+            {
+                for(int j = i+1; j < listaEnteros.Count; j++)
+                {
+                    if(listaEnteros[i] > listaEnteros[j])
+                    {
+                        // Intercambia valores
+                        aux = listaEnteros[i];
+                        listaEnteros[i] = listaEnteros[j];
+                        listaEnteros[j] = aux;
+
+                    }
+                }
+            }
+            for (int i = 0; i < listaEnteros.Count ; i++)
+                Console.WriteLine(listaEnteros[i]);
+        }
+
+
+        public void pausa()
+        {
+            Console.WriteLine("Pulsa una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ListasMartes13/ListasMartes13/Program.cs
@@ -0,0 +1,24 @@
+﻿     using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasMartes13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Lista de Enteros y ordenar
+            // 1.- Instanciar.
+            Class1 class1 = new Class1();
+            List<int> ListaEnteros = new List<int>();
+            // 2.- Llamada a métodos
+            class1.PedirNumeros(ListaEnteros);            
+            class1.ComprobarLista(ListaEnteros);
+            class1.pausa();
+        }
+
+        
+    }
+}
